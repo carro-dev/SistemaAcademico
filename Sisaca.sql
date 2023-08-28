8112413acc87bb02f5d4e108a63b15b7a25d420d
Aliany González <alianygonzalez21@gmail.com>
2023-08-28 00:55:02 -0400

    Update Sisaca.sql

--- Sisaca.sql
+++ Sisaca.sql
@@ -12,7 +12,7 @@ primary key (id_docente)
 );
 
 Create table TblTipoMateria (
-id_tipomateria int auto_increment not null,
+id_tipomateria int auto_increment,
 tipo_materia varchar(10) not null,
 primary key (id_tipomateria)
 );
@@ -24,7 +24,7 @@ primary key (codigo_area)
 );
 
 Create table TblRoles (
-id_rol int auto_increment not null,
+id_rol int auto_increment,
 rol varchar(15) not null,
 primary key (id_rol)
 );
@@ -38,7 +38,7 @@ foreign key (codigo_area) references TblAreas(codigo_area)
 );
 
 Create table TblUsuarios (
-id_usuario int auto_increment not null,
+id_usuario int auto_increment,
 id_rol int not null,
 clave varchar(64) not null,
 primary key (id_usuario),
@@ -49,10 +49,11 @@ Create table TblEstudiantes (
 matricula char(7) not null,
 id_usuario int not null,
 codigo_carrera char(3) not null,
-nombre_estudiante varchar(50),
-apellido_estudiante varchar(50),
+nombre_estudiante varchar(50) not null,
+apellido_estudiante varchar(50) not null,
 primary key (matricula),
-foreign key (id_usuario) references TblUsuarios(id_usuario)
+foreign key (id_usuario) references TblUsuarios(id_usuario),
+foreign key (codigo_carrera) references TblCarreras(codigo_carrera)
 );
 
 Create table TblAdmin (
@@ -76,11 +77,11 @@ foreign key (codigo_carrera) references TblCarreras(codigo_carrera)
 );
 
 Create table TblSecciones (
-id_seccion int auto_increment not null,
+id_seccion int auto_increment,
 numero_seccion int not null,
 codigo_materia char(7) not null,
 id_docente int not null,
-horario json,
+horario json not null,
 primary key (id_seccion),
 foreign key (codigo_materia) references TblMaterias(codigo_materia),
 foreign key (id_docente) references TblDocentes(id_docente)
@@ -102,6 +103,7 @@ foreign key (codigo_carrera) references TblCarreras(codigo_carrera),
 foreign key (codigo_materia) references TblMaterias(codigo_materia)
 );
 
+alter table TblCarreras add column max_creditos int after nombre_carrera;
 
 -- PROCEDURES --
 
@@ -120,14 +122,16 @@ SPapellido_docentes);
 end$$
 
 Create procedure SP_InsertarCarreras (IN SPcodigo_carrera char(3)
-, IN SPnombre_carrera varchar(50))
+, IN SPnombre_carrera varchar(50), IN SPmax_creditos int)
 begin
 insert into Tbl_Carreras (
 codigo_carrera,
-nombre_carrera)
+nombre_carrera,
+max_creditos)
 values (
 SPcodigo_carrera,
-SPnombre_carrera);
+SPnombre_carrera,
+SPmax_creditos);
 end$$
 
 Create procedure SP_InsertarTipoMateria (IN SPtipo_materia varchar(10))
@@ -169,10 +173,100 @@ SPclave);
 end$$
 
 Create procedure SP_InsertarEstudiantes (IN SPmatricula char(7),
-IN SPid_usuario int, IN SPnombre_estudiante varchar(50), IN SPapellido_estudiante varchar(50))
+IN SPid_usuario int, IN SPcodigo_carrera char(3), IN SPnombre_estudiante varchar(50), IN SPapellido_estudiante varchar(50))
 begin
 insert into Tbl_Estudiantes (
 matricula,
 id_usuario,
-nombre_estudiante
-apellido_estudiante
+codigo_carrera,
+nombre_estudiante,
+apellido_estudiante)
+values (
+SPmatricula,
+SPid_usuario,
+SPcodigo_carrera,
+SPnombre_estudiante,
+SPapellido_estudiante);
+end$$
+
+Create procedure SP_InsertarAdmin (IN SPcodigo_admin char(5), 
+IN SPid_usuario int)
+begin
+insert into Tbl_Admin (
+codigo_admin,
+id_usuario)
+values (
+SPcodigo_admin,
+SPid_usuario);
+end$$
+
+Create procedure SP_InsertarMaterias (IN SPcodigo_materia char(7), 
+IN SPnombre_materia varchar(50), IN SPid_tipomateria int, IN SPcodigo_area char(3), IN SPcodigo_carrera char(3), IN SPcreditos int)
+begin
+insert into Tbl_Materias (
+codigo_materia,
+nombre_materia,
+id_tipomateria,
+codigo_area,
+codigo_carrera,
+creditos)
+values (
+SPcodigo_materia,
+SPnombre_materia,
+SPid_tipomateria,
+SPcodigo_area,
+SPcodigo_carrera,
+SPcreditos);
+end$$
+
+Create procedure SP_InsertarSecciones (IN SPnumero_seccion int, IN SPcodigo_materia char(7), IN SPid_docente int, IN SPhorario json)
+begin
+insert into Tbl_Secciones (
+numero_seccion,
+codigo_materia,
+id_docente,
+horario)
+value (
+SPnumero_seccion,
+SPcodigo_materia,
+SPid_docente,
+SPhorario);
+end$$
+
+Create procedure SP_InsertarAlumnosSeccion (IN SPmatricula char(7), 
+IN SPid_seccion int)
+begin
+insert into Tbl_AlumnosSeccion (
+matricula,
+id_seccion)
+values (
+SPmatricula,
+SPid_seccion);
+end$$
+
+Create procedure SP_InsertarMateriasEnCarrera (IN SPcodigo_carrera char(3),
+IN SPcodigo_materia char(7))
+begin
+insert into Tbl_MateriasEnCarrera (
+codigo_carrera,
+codigo_materia)
+values (
+SPcodigo_carrera,
+SPcodigo_materia);
+end$$
+
+-- UPDATE --
+
+Create procedure SP_EditarDocentes (IN SPid_docente int, IN SPnombre_docente varchar(50)
+, IN SPapellido_docente varchar(50))
+begin
+update TblDocentes 
+set nombre_docente = SPnombre_docente, apellido_docente = SPapellido_docente where id_docente = SPid_docente;
+end$$
+
+Create procedure SP_EditarCarreras (IN SPcodigo_carrera char(3)
+, IN SPnombre_carrera varchar(50), IN SPmax_creditos int)
+begin
+update TblCarreras
+set codigo_carrera = SPcodigo_carrera, nombre_carrera = SPnombre_carrera, max_creditos = SPmax_creditos) where codigo_carrera = SPcodigo_carrera
+
